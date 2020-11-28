32b0568bb31d7810ae2523b4c0d0d4703b67f3f5
육심현 <simoncreek@tutanota.com>
2020-11-28 23:22:15 +0900

    Added start for template

---
+++ src/template.js
@@ -0,0 +1,18 @@
+'use strict;'
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+	GetTemplateContent(path) {
+		// TODO :: Check if given path exists
+		// Read file content and return as string
+	}
+
+	ExportTemplate(content, path) {
+		// TODO :: Make content string into 
+		// given filename 
+		// If file name is given fully then save to iven extension.
+		// if file name is given without extension add .md
+	}
+}
